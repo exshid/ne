0a8d768d628579ae2c6ac330bc8e1abb83e18f61
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2023-10-05 17:48:08 +0330

    drawer

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 'use client';
 import React from 'react';
 import { Button } from 'antd';
-
+import Drawer from '../components/drawer'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
     <Button type="primary">Button</Button>
-
+<Drawer/>
     </main>
   )
 }
