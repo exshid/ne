f2b42e6d5fb993eacc5100eadf2be6669322b185
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2023-10-05 21:30:13 +0330

    header

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} transition font-ysabeau`}>{children}</body>
+      <body className='transition font-ysabeau'>{children}</body>
     </html>
   )
 }
