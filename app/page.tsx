6df05d50cfe9c601875001137e839db07fe6c19d
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2023-10-05 17:57:54 +0330

    drawer

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,64 @@
 'use client';
 import React from 'react';
-import { Button } from 'antd';
-import DrawerComponent from '../components/drawer'
+import { Breadcrumb, Layout, Menu, theme } from 'antd';
+const { Header, Content, Footer } = Layout;
+
 export default function Home() {
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    <Button type="primary">Button</Button>
-<DrawerComponent/>
-    </main>
-  )
-}
+    <Layout className="layout">
+      <Header
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+        }}
+      >
+        <div className="demo-logo" />
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={['2']}
+          items={new Array(15).fill(null).map((_, index) => {
+            const key = index + 1;
+            return {
+              key,
+              label: `nav ${key}`,
+            };
+          })}
+        />
+      </Header>
+      <Content
+        style={{
+          padding: '0 50px',
+        }}
+      >
+        <Breadcrumb
+          style={{
+            margin: '16px 0',
+          }}
+        >
+          <Breadcrumb.Item>Home</Breadcrumb.Item>
+          <Breadcrumb.Item>List</Breadcrumb.Item>
+          <Breadcrumb.Item>App</Breadcrumb.Item>
+        </Breadcrumb>
+        <div
+          className="site-layout-content"
+          style={{
+            background: colorBgContainer,
+          }}
+        >
+          Content
+        </div>
+      </Content>
+      <Footer
+        style={{
+          textAlign: 'center',
+        }}
+      >
+        Ant Design ©2023 Created by Ant UED
+      </Footer>
+    </Layout>
+  );
+};
