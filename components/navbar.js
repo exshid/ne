241885c0f85a836cd006688458e486aa8fcf298c
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2023-10-06 17:48:39 +0330

    header

--- components/navbar.js
+++ components/navbar.js
@@ -5,9 +5,9 @@ const Navbar = () => {
     const [isOpen, setIsOpen] = useState(false);
 
     return (
-<header className="sticky top-0 font-ysabeau text-[#1E1E1E]">
+<header className="sticky top-0 font-ysabeau z-[555] text-[#1E1E1E]">
 <nav className="box-shadow-custom bg-white w-full">
-            <div className="container py-6 mx-auto flex items-center justify-between">
+            <div className="container p-6 mx-auto flex items-center justify-between">
                 <div className="text-lg font-bold">My Website</div>
                 <div className={`lg:flex hidden space-x-6 ${isOpen ? 'block' : 'hidden'}`}>
                     <a href="#">Home</a>
@@ -16,12 +16,7 @@ const Navbar = () => {
                     <a href="#">Contact</a>
                 </div>
                 <div className="lg:hidden block">
-                    <button
-                        onClick={() => setIsOpen(!isOpen)}
-                        className="text-white focus:outline-none"
-                    >
-                        III
-                    </button>
+                <svg xmlns="http://www.w3.org/2000/svg" className="w-[30px] h-[30px] lg:h-[25px] lg:w-[25px]" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16"></path></svg>
                 </div>
             </div>
         </nav>
