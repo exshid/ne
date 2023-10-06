896e27e88a368d199c1fdbb321d3cd6b634c2545
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2023-10-06 16:29:01 +0330

    card

--- components/books.js
+++ components/books.js
@@ -6,6 +6,7 @@ import { Col, Row } from 'antd';
 
 const booksArray = [
   { title: 'Book 1', author: 'Author 1', imageUrl: 'https://ca-times.brightspotcdn.com/dims4/default/f9acfb8/2147483647/strip/true/crop/1200x1813+0+0/resize/1200x1813!/quality/75/?url=https%3A%2F%2Fcalifornia-times-brightspot.s3.amazonaws.com%2Faf%2F5a%2F2f3a70a244b08aff8571dda5a3e8%2Fbooks-hunger-games-54386.jpg' },
+  { title: 'Book 3', author: 'Author 3', imageUrl: 'https://cdn.kobo.com/book-images/a821b502-0d07-4921-ac14-e431625d04e7/1200/1200/False/the-hobbit-illustrated-by-alan-lee.jpg' },
   { title: 'Book 2', author: 'Author 2', imageUrl: 'https://m.media-amazon.com/images/I/71lKy8RoFUL._AC_UF1000,1000_QL80_.jpg' },
   { title: 'Book 3', author: 'Author 3', imageUrl: 'https://cdn.kobo.com/book-images/a821b502-0d07-4921-ac14-e431625d04e7/1200/1200/False/the-hobbit-illustrated-by-alan-lee.jpg' },
   { title: 'Book 4', author: 'Author 4', imageUrl: 'https://ca-times.brightspotcdn.com/dims4/default/f9acfb8/2147483647/strip/true/crop/1200x1813+0+0/resize/1200x1813!/quality/75/?url=https%3A%2F%2Fcalifornia-times-brightspot.s3.amazonaws.com%2Faf%2F5a%2F2f3a70a244b08aff8571dda5a3e8%2Fbooks-hunger-games-54386.jpg' }
@@ -31,13 +32,14 @@ const Books = () => {
 
   return (
 <>
-<div className="text-white font-ysabeau">
-<h3 className="font-black pt-5 text-2xl md:text-3xl lg:text-5xl font-black uppercase bg-gradient-to-r from-rose-600 to-rose-900 bg-clip-text text-transparent">
-    Meet our Agents</h3>
+<div className="text-white font-ysabeau py-6 flex flex-col items-center font-ysabeau bg-gradient-to-r from-emerald-400 to-cyan-400">
+<h3 className="font-black p-6 text-2xl md:text-3xl lg:text-5xl font-black uppercase">
+    Some of our books</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
     <div className="flex flex-wrap odd:mr-[5px]">
       {booksArray.map((book, index) => (
         <>
-        <div className="w-full md:w-1/2 bg-black lg:w-1/4" key={index} onClick={() => setBookNumber(book.title)}  >
+        <div className="w-full md:w-1/2 lg:w-1/4" key={index} onClick={() => setBookNumber(book.title)}  >
           <div className="rounded-lg shadow-lg h-full">
         <BookCard title={book.title} author={book.author} imageUrl={book.imageUrl} onDrawer={showDrawer} />
           </div>
