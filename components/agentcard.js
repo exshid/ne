0ace076045dad40581823b1b8de4cd9cec070689
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2023-10-08 01:09:39 +0330

    image

--- components/agentcard.js
+++ components/agentcard.js
@@ -1,14 +1,16 @@
+import Image from 'next/image'
 import React from 'react';
 import {Card, Button, Space } from 'antd';
 const AgentCard = ({ name, title, image, onDrawer }) => (
   <>
 <div onClick={onDrawer} className="relative cursor-pointer w-full p-6 h-96 group">
-      <img
-        className="absolute inset-0 object-cover w-full h-full"
-        src={image}
-        alt={`photo of ${name}`}
-        />
-      <div className="absolute inset-x-0 bottom-0 px-4 pb-6 group-hover:pb-8 transition pt-32 bg-gradient-to-t from-black to-transparent">
+<Image
+      src={image}
+      alt={`photo of ${name}`}
+      className="absolute inset-0 object-cover w-full h-full"
+    />
+
+       <div className="absolute inset-x-0 bottom-0 px-4 pb-6 group-hover:pb-8 transition pt-32 bg-gradient-to-t from-black to-transparent">
         <h3 className="text-lg font-bold text-white group-hover:-translate-y-[4px] transition">{name}</h3>
         <p className="text-slate-600 group-hover:-translate-y-[4px] transition">{title}</p>
       </div>
