189aeb06a3325fb1cf5347536bed28a97c2f11fa
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2023-10-08 01:23:26 +0330

    images

--- app/policies/page.js
+++ app/policies/page.js
@@ -1,5 +1,4 @@
 'use client'
-import {Contact} from '../../components/contact';
 import Image from 'next/image'
 import { blogConfig } from '@/config';
 import Agents from '../../components/agents';
@@ -56,14 +55,14 @@ export default function About() {
               }}
             >
     
-<div className="flex h-full items-center flex-col w-full py-24 lg:pt-40">
+<div className="flex h-full items-center flex-col w-full py-20 lg:pt-40">
       <h2 className="font-black px-2 md:px-5 lg:px-10 mb-10 xl:px-80 text-3xl md:text-4xl lg:text-5xl font-ysabeau bg-gradient-to-r from-rose-600 to-rose-900 bg-clip-text text-transparent [55px]">Privacy Policy</h2>
       <div       style={{  background: `url('https://unsplash.com/photos/p_kICQCOM4s/download?ixid=M3wxMjA3fDB8MXxzZWFyY2h8MTV8fG9mZmljZXxlbnwwfHx8fDE2OTY1MjIzMzZ8MA&force=true&w=1920')`,
       backgroundSize: 'cover',
       backgroundAttachment: 'fixed'}}
  className="relative lg:rounded-2xl mb-10 w-full lg:w-9/12 flex flex-col lg:flex-row justify-between my-2 bg-gradient-to-r from-rose-600 to-rose-900 h-[60vh]">
       </div>
-      <div className="w-full px-2 md:px-5 lg:px-10 xl:px-80 h-full pb-2 self-start text-center"><p className="inline-block text-3xl md:text-4xl lg:text-5xl mr-5 font-ysabeau text-gray-600 font-black heading-font">We value your privacy</p>
+      <div className="w-full px-2 md:px-5 lg:px-10 xl:px-80 h-full self-start text-center"><p className="inline-block text-3xl md:text-4xl lg:text-5xl mr-5 font-ysabeau text-gray-600 font-black heading-font">We value your privacy</p>
 </div>
 <div className="px-2 md:px-5 lg:px-10 xl:px-80 w-full font-ysabeau text-gray-600 items-center jakarta-font">
     <p className="py-2 whitespace-pre-wrap">{blogConfig.about}</p>
