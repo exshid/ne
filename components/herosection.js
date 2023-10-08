3629f054212409a37089bfce79522b61a6a4c728
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2023-10-08 18:09:29 +0330

    images

--- components/herosection.js
+++ components/herosection.js
@@ -4,17 +4,17 @@ import Link from 'next/link';
 const HeroSection = () => {
   return (
     <div className="h-screen flex flex-col justify-center items-center bg-darkish text-white font-ysabeau" >
-      <h1 className="text-2xl md:text-6xl mb-4 text-white">WELCOME TO</h1>
-      <h2 className="text-4xl md:text-8xl mb-6 bg-gradient-to-r from-rose-600 to-rose-900 bg-clip-text text-transparent font-black">OUR AGENCY</h2>
+      <h1 className="text-4xl sm:text-6xl mb-4 text-white">WELCOME TO</h1>
+      <h2 className="text-6xl sm:text-8xl mb-6 bg-gradient-to-r from-rose-600 to-rose-900 bg-clip-text text-transparent font-black">OUR AGENCY</h2>
       <div className="flex space-x-4 mb-8 w-full md:w-auto">
-      <div className="transition hover:-translate-y-[4px] pb-2 pt-4">
+      <div className="transition hover:-translate-y-[4px] pb-2 pt-4 w-1/2 md:w-auto">
                       <Link href="about">  <button 
-     className="uppercase block transition w-full p-3 w-1/2 md:w-auto md:px-16 text-lg text-white rounded bg-gradient-to-r from-rose-600 to-rose-900 focus:outline-none">About Us</button>
+     className="uppercase block transition w-full p-3 md:px-16 text-lg text-white rounded bg-gradient-to-r from-rose-600 to-rose-900 focus:outline-none">About Us</button>
      </Link>
                     </div>
-        <div className="transition hover:-translate-y-[4px] pb-2 pt-4">
+        <div className="transition hover:-translate-y-[4px] pb-2 pt-4 w-1/2 md:w-auto">
                       <Link href="#our-team">  <button 
-     className="uppercase block transition w-full p-3 w-1/2 md:w-auto md:px-16 text-lg text-white rounded bg-gradient-to-r from-rose-600 to-rose-900 focus:outline-none">Our Team</button>
+     className="uppercase block transition w-full p-3 md:px-16 text-lg text-white rounded bg-gradient-to-r from-rose-600 to-rose-900 focus:outline-none">Our Team</button>
      </Link>
                     </div>
       </div>
