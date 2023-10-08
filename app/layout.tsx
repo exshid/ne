23640862693ee0559ef55b5740803ce0a4586b08
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2023-10-09 03:00:20 +0330

    layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,12 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import Navbar from '../components/navbar';
+import FooterSection from '../components/footer';
+import {Contact} from '../components/contact';
+import PageWrapper from '@/components/wrapper';
 
-const inter = Inter({ subsets: ['latin'] })
+
+
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -16,7 +20,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className='transition font-ysabeau'>{children}</body>
+      <body className='transition font-ysabeau'>{children}
+
+      <Contact/>
+
+      </body>
     </html>
   )
 }
