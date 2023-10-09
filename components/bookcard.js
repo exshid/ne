6f6d43527baab4582e69d9079d9407b2acd80790
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2023-10-09 04:23:58 +0330

    layout

--- components/bookcard.js
+++ components/bookcard.js
@@ -12,9 +12,9 @@ const BookCard = ({ title, author, imageUrl}) => (
     <div className="absolute bottom-0 left-0 w-full p-6 pt-32 bg-gradient-to-t from-black to-transparent">
       <h2 aria-hidden="true" className="text-white text-xl font-bold">{title}</h2>
       <h2 className="sr-only">A book titled {title}</h2>
-      <p className="sr-only">{author}</p>
+      <p className="sr-only">Written by an author named {author}</p>
 
-      <p aria-hidden="true" className="text-[gray] text-sm">Written by an author named {author}</p>
+      <p aria-hidden="true" className="text-[gray] text-sm">{author}</p>
     </div>
   </div>
 );
